ccc039e77acf2451daec53d3abd102a756a6b64e
kabi239 <kavyasharma239@gmail.com>
2024-12-19 01:29:02 +0530

    Validation added to Product Model

--- src/main/java/com/ecommerce/sb_ecom/controller/ProductController.java
+++ src/main/java/com/ecommerce/sb_ecom/controller/ProductController.java
@@ -3,6 +3,7 @@ package com.ecommerce.sb_ecom.controller;
 import com.ecommerce.sb_ecom.payload.ProductDTO;
 import com.ecommerce.sb_ecom.payload.ProductResponse;
 import com.ecommerce.sb_ecom.service.ProductService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -20,7 +21,7 @@ public class ProductController {
     ProductService productService;
 
     @PostMapping("/admin/categories/{categoryId}/product")
-    public ResponseEntity<ProductDTO> addProduct(@RequestBody ProductDTO productDTO, @PathVariable Long categoryId){
+    public ResponseEntity<ProductDTO> addProduct(@Valid @RequestBody ProductDTO productDTO, @PathVariable Long categoryId){
         ProductDTO savedproductDTO = productService.addProduct(categoryId,productDTO);
         return new ResponseEntity<>(productDTO, HttpStatus.CREATED);
     }
@@ -44,7 +45,7 @@ public class ProductController {
     }
 
     @PutMapping("/admin/products/{productId}")
-    public ResponseEntity<ProductDTO> updateProduct(@RequestBody ProductDTO productDTO, @PathVariable Long productId){
+    public ResponseEntity<ProductDTO> updateProduct(@Valid @RequestBody ProductDTO productDTO, @PathVariable Long productId){
         ProductDTO updateProductDTO=productService.updateProduct(productDTO,productId);
         return new ResponseEntity<>(updateProductDTO, HttpStatus.OK);
     }
